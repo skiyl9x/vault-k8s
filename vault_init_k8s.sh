943a0e95ddd202537ba2ea6425a83cb1f5c98b27
skiyl9x <skiyl9x@gmail.com>
2022-02-19 17:00:36 +0200

    fix bugs

--- vault_init_k8s.sh
+++ vault_init_k8s.sh
@@ -56,7 +56,7 @@ function init_vault {
       echo -e "\n${YELLOW}* Init vault${NC}"
       kubectl -n $NAMESPACE exec vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-keys.json
       echo -e "- Vault keys have writen in cluster-keys.json."
-      echo -e "${YELLOW}- Save this keys into SECRET STORAGE and delete file.{NC}"
+      echo -e "${YELLOW}- Save this keys into SECRET STORAGE and delete file.${NC}"
     else 
       echo -e "\nVault was already initialized"
     fi
